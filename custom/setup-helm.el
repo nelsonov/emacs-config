80b6250ce841dde298c9ec44d184614bcaeaa393
Lee Nelson <lnelson@nelnet.org>
2020-09-23 15:06:58 -0700

    Remove helm mode for find file

--- custom/setup-helm.el
+++ custom/setup-helm.el
@@ -11,7 +11,7 @@
 
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
+;;(global-set-key (kbd "C-x C-f") 'helm-find-files)
 
 (global-set-key (kbd "C-x b") 'helm-mini)
 (setq helm-buffers-fuzzy-matching t
