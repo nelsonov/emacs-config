d17c41709d90aa537019dd4ee9d71f1a9ceceb5b
Lee Nelson <lnelson@nelnet.org>
2020-10-11 15:46:16 -0700

    Add indent highlight to yaml mode

--- custom/setup-yaml.el
+++ custom/setup-yaml.el
@@ -1,6 +1,8 @@
 (use-package yaml-mode
   :ensure t
   :mode (("\\.yaml\\'" . yaml-mode)
-	 ("\\.yml\\'" . yaml-mode)))
+	 ("\\.yml\\'" . yaml-mode))
+  :config
+  (add-hook 'yaml-mode-hook 'highlight-indent-guides-mode))
 
 (provide 'setup-yaml)
