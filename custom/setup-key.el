2481ca123070b985ae81e458d2c343d07fb0b18d
Lee Nelson <lnelson@nelnet.org>
2021-07-24 13:14:13 -0700

    Remove C-c c compile binding

--- custom/setup-key.el
+++ custom/setup-key.el
@@ -1,3 +1,10 @@
+;; See also:
+;; setup-helm.el
+;; setup-ibuffer.el
+;; setup-org.el
+;; setup-spelling.el
+;; setup-utf8.el
+;;
 (global-set-key (kbd "C-?") `help-command)
 (global-set-key (kbd "M-?") `mark-paragraph)
 (global-set-key (kbd "C-h") `delete-backward-char)
@@ -5,5 +12,6 @@
 (global-set-key (kbd "C-c g") 'goto-line)
 (global-set-key [f9]    'goto-line); F9
 (global-set-key (kbd "RET") 'newline-and-indent)
-(global-set-key (kbd "C-c c") 'compile)
+;(global-set-key (kbd "C-c c") 'compile) ; conflicts with org-mode bindings
 (provide 'setup-key)
+
