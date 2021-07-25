209da9d5fb72d49d6473926fa411230de0b16ced
Lee Nelson <lnelson@nelnet.org>
2021-07-24 21:15:56 -0700

    Add key bindings

--- custom/setup-key.el
+++ custom/setup-key.el
@@ -10,6 +10,8 @@
 (global-set-key (kbd "C-h") `delete-backward-char)
 (global-set-key (kbd "M-h") `backward-kill-word)
 (global-set-key (kbd "C-c g") 'goto-line)
+(global-set-key (kbd "C-c r") 'replace-string)
+(global-set-key (kbd "C-c x") 'replace-regex)
 (global-set-key [f9]    'goto-line); F9
 (global-set-key (kbd "RET") 'newline-and-indent)
 ;(global-set-key (kbd "C-c c") 'compile) ; conflicts with org-mode bindings
